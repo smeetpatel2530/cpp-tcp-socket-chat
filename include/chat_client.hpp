2847902c93bb7023ef92decdc8520dce58dc9848
SMEET PATEL <60186652+smeetpatel2530@users.noreply.github.com>
2026-05-23 14:57:27 +0530

    Refactor ChatClient constructor and member variables

--- include/chat_client.hpp
+++ include/chat_client.hpp
@@ -1,23 +1,26 @@
 #pragma once
 #include "logger.hpp"
+#include "platform.hpp"
 #include <atomic>
 #include <string>
 #include <thread>
 
 class ChatClient {
 public:
-    ChatClient(const std::string& host, int port, const std::string& username, const std::string& log_file);
+    ChatClient(const std::string& host, int port,
+               const std::string& username,
+               const std::string& log_file);
     ~ChatClient();
     void run();
 private:
     void receive_loop();
     void send_line(const std::string& line);
 
-    std::string host_;
-    int port_;
-    std::string username_;
-    int socket_fd_;
+    std::string  host_;
+    int          port_;
+    std::string  username_;
+    socket_t     socket_fd_;
     std::atomic<bool> running_;
-    Logger logger_;
-    std::thread receiver_;
+    Logger       logger_;
+    std::thread  receiver_;
 };
